7e2e40695cfd0983c085844c2ced2827e027fc64
johnlok <john.lok.djinn@gmail.com>
2014-11-13 18:59:56 +0800

    created some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Item.create title:"Dirt", description:"Free dirt. Get your hands in some dirt that has a lot of different types of minerals in it.", location:"Wan Chai", expiry_date:"2014-12-09", lister_email:"congue.elit.sed@lorem.net", lister_tel:"8334-4020", sms_notify:TRUE
+Item.create title:"Gold", description:"Gold! One nanogram of gold available for free.", location:"Wan Chai", expiry_date:"2014-12-12", lister_email:"Quisque.fringilla.euismod@Nuncacsem.edu", lister_tel:"3402-4209", sms_notify:TRUE
+Item.create title:"Ikea Couch", description:"Ikea couch. Barely used. Owned by a group of well behaved college students.", location:"Central", expiry_date:"2014-12-02", lister_email:"mattis@cursusNuncmauris.org", lister_tel:"5763-2600", sms_notify:TRUE
+Item.create title:"Hairdryer", description:"This hairdryer will quickly dry your hair.", location:"Kowloon Tong", expiry_date:"2014-11-17", lister_email:"mauris.a@fermentumrisus.edu", lister_tel:"7250-9462", sms_notify:FALSE
+Item.create title:"Iphone 5S", description:"Iphone 5s that has a cracked screen (still functional)", location:"Sai Ying Pun", expiry_date:"2014-11-20", lister_email:"Nunc@Pellentesqueultriciesdignissim.co.uk", lister_tel:"4037-0878", sms_notify:FALSE
+Item.create title:"Baby Clothes", description:"Barely used baby clothes suitable for babies who are from 1 years to 2 years old.", location:"Choi Hung", expiry_date:"2014-11-26", lister_email:"natoque.penatibus@dapibusligulaAliquam.net", lister_tel:"5510-2338", sms_notify:TRUE
+Item.create title:"Handbag", description:"A handbag which was owned by my grandmother.", location:"Sai Kung", expiry_date:"2014-11-29", lister_email:"fringilla.purus@enim.ca", lister_tel:"3625-3057", sms_notify:TRUE
+Item.create title:"Playstation 4", description:"Free playstation 4 for anyone who wants it.", location:"Shatin", expiry_date:"2014-12-04", lister_email:"orci@arcu.com", lister_tel:"4901-4454", sms_notify:FALSE
+Item.create title:"Xbox 360", description:"Xbox 360 - comes with several used games.", location:"Tai Po", expiry_date:"2014-12-08", lister_email:"varius.et.euismod@velarcuCurabitur.com", lister_tel:"2518-7106", sms_notify:FALSE
+Item.create title:"Nokia Phone", description:"Here's a nokia phone I no longer need. Includes the game Snake", location:"Tai Wai", expiry_date:"2014-12-11", lister_email:"magna.sed@elitpellentesque.com", lister_tel:"7189-3502", sms_notify:FALSE
+Item.create title:"Typewriter", description:"An old typewriter which is will be in fashion soon.", location:"Tin Shui Wai", expiry_date:"2014-12-14", lister_email:"cursus@Suspendisse.edu", lister_tel:"7493-3179", sms_notify:TRUE
